c739352f5d22da27667b99b7c1e737f610087f63
Darick33 <sebolin2004@gmail.com>
2024-02-23 23:32:01 -0500

    feat: add login

--- controllers/usuarios.controllers.php
+++ controllers/usuarios.controllers.php
@@ -1,7 +1,7 @@
 <?php
 
 error_reporting(0);
-
+require_once('../config/sesiones.php');
 require_once('../models/usuarios.models.php');
 $Usuarios = new Usuarios();
 
@@ -47,6 +47,55 @@ switch ($_GET['op']) {
         $datos = $Usuarios->eliminar($UserID);
         echo json_encode($datos);
         break;
+        case 'login':
+            $correo = $_POST['correo'];
+            $contrasenia = $_POST['contrasenia'];
+    
+            //TODO: Si las variables estab vacias rgersa con error
+            if (empty($correo) or  empty($contrasenia)) {
+                header("Location:../login.php?op=2");
+                exit();
+            }
+    
+            try {
+                $datos = array();
+                $datos = $Usuarios->login($correo, $contrasenia);
+                $res = mysqli_fetch_assoc($datos);
+            } catch (Throwable $th) {
+                header("Location:../login.php?op=1");
+                exit();
+            }
+            //TODO:Control de si existe el registro en la base de datos
+            try {
+                if (is_array($res) and count($res) > 0) {
+                    //if ((md5($contrasenia) == ($res["Contrasenia"]))) {
+                    if ((($contrasenia) == ($res["Contrasenia"]))) {
+                        //$datos2 = array();
+                        // $datos2 = $Accesos->Insertar(date("Y-m-d H:i:s"), $res["idUsuarios"], 'ingreso');
+    
+                        $_SESSION["idUsuarios"] = $res["idUsuarios"];
+                        $_SESSION["Usuarios_Nombres"] = $res["Nombres"];
+                        $_SESSION["Usuarios_Apellidos"] = $res["Apellidos"];
+                        $_SESSION["Usuarios_Correo"] = $res["Correo"];
+                        $_SESSION["Usuario_IdRoles"] = $res["idRoles"];
+                        $_SESSION["Rol"] = $res["Rol"];
+    
+    
+    
+                        header("Location:../views/home.php");
+                        exit();
+                    } else {
+                        header("Location:../login.php?op=1");
+                        exit();
+                    }
+                } else {
+                    header("Location:../login.php?op=1");
+                    exit();
+                }
+            } catch (Exception $th) {
+                echo ($th->getMessage());
+            }
+            break;    
 }
 
 ?>
