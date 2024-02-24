65f24e14893055d4e2d1b7ed78afd5e53eae8762
Edison Gonzalez <139730204+EdisonGonzalezA@users.noreply.github.com>
2024-02-23 23:04:50 -0500

    feat: de tareas MVC

---
+++ views/html/tareas/tareas.views.php
@@ -0,0 +1,76 @@
+<?php require_once('../html/head2.php')  ?>
+
+<!-- Basic Bootstrap Table -->
+<div class="card">
+    <button type="button" class="btn btn-outline-secondary" onclick="tareas();" data-bs-toggle="modal" data-bs-target="#ModalTareas">Nueva Tarea</button>
+
+
+    <h5 class="card-header">Lista de Tareas</h5>
+    <div class="table-responsive text-nowrap">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Descripcion</th>
+                    <th>FechaCreacion</th>
+                    <th>FechaVencimiento</th>
+                    <th>Estado</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody class="table-border-bottom-0" id="ListaTareas">
+
+            </tbody>
+        </table>
+    </div>
+</div>
+
+
+<!-- Modal Sucursales-->
+
+<div class="modal" tabindex="-1" id="ModalTareas">
+    <div class="modal-dialog modal-dialog-centered modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="tituloModal"></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <form id="form_tareas" method="post">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="Nombres">Descripción</label>
+                        <input type="text" name="Descripcion" id="Descripcion" class="form-control" placeholder="Ingrese una Descripción" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="Nombres">Fecha de Creación</label>
+                        <input type="date" name="FechaCreacion" id="FechaCreacion" class="form-control" placeholder="Ingrese una fecha de inicio" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="Nombres">Fecha de Vencimiento</label>
+                        <input type="date" name="FechaVencimiento" id="FechaVencimiento" class="form-control" placeholder="Ingrese una fecha aproximada de termino" required>
+                    </div>                    
+                    <div class="form-group">
+                        <label for="Nombres">Estado</label>
+                        <input type="text" name="Estado" id="Estado" class="form-control" placeholder="Estado" required>
+                    </div>
+                    
+                    
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Guardar</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+<?php require_once('../html/scripts2.php') ?>
+
+<script src="./tareas.views.js"></script>
